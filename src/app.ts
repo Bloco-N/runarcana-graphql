281102c91f337daf24df0de68acae5b49c1699ae
iueu <38703179+Ilwel@users.noreply.github.com>
2022-07-02 16:19:00 -0300

    app module

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+// index.ts
+import 'reflect-metadata'
+import * as dontenv from 'dotenv'
+import * as tq from 'type-graphql'
+import { ApolloServer } from 'apollo-server'
+import { context } from './context'
+import resolvers from './resolvers'
+import { Auth } from './schemas/Auth'
+import Authentication from './middlewares/Authentication'
+dontenv.config()
+
+const app = async (port, log = true) => {
+  const schema = await tq.buildSchema({
+    resolvers: [
+      Auth,
+      ...resolvers
+    ],
+    authChecker: Authentication,
+    emitSchemaFile: true
+  })
+
+  const server = new ApolloServer({
+    schema,
+    context
+  })
+  server.listen({ port }, () => {
+    if (log) console.log(`👌 Server ready at: http://localhost:${port}`)
+  }
+  )
+  return server
+}
+
+export { app }
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,6 @@
-// index.ts
-import 'reflect-metadata'
 import * as dontenv from 'dotenv'
-import * as tq from 'type-graphql'
-import { ApolloServer } from 'apollo-server'
-import { context } from './context'
-import resolvers from './resolvers'
-import { Auth } from './schemas/Auth'
-import Authentication from './middlewares/Authentication'
+import { app } from './app'
 dontenv.config()
 const { PORT } = process.env
 
-const app = async () => {
-  const schema = await tq.buildSchema({
-    resolvers: [
-      Auth,
-      ...resolvers
-    ],
-    authChecker: Authentication,
-    emitSchemaFile: true
-  })
-
-  return new ApolloServer({
-    schema,
-    context
-  }).listen({ port: PORT }, () =>
-    console.log(`👌 Server ready at: http://localhost:${PORT}`)
-  )
-}
-
-app()
+app(PORT)
